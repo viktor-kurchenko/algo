0ad348939257c661437a3dc07d09212d11322143
ViktorKurchenko <extvikur@pandora.net>
2022-10-05 08:46:30 +0300

    LinkedList cycle length.

--- 3-fast-slow-pointers/fast_slow_pointers.go
+++ 3-fast-slow-pointers/fast_slow_pointers.go
@@ -11,6 +11,23 @@ func LinkedListCycle(list *Node) bool {
 	return false
 }
 
+func LinkedListCycleLength(list *Node) int {
+	slow, fast := list, list
+	for {
+		slow, fast = slow.next, fast.next.next
+		if slow == fast {
+			break
+		}
+	}
+	n := slow.next
+	i := 1
+	for n != slow {
+		i++
+		n = n.next
+	}
+	return i
+}
+
 // helpers
 
 type Node struct {
--- 3-fast-slow-pointers/fast_slow_pointers_test.go
+++ 3-fast-slow-pointers/fast_slow_pointers_test.go
@@ -29,3 +29,25 @@ func TestLinkedListCycle(t *testing.T) {
 	node6.next = list.next.next
 	assert.True(t, LinkedListCycle(&list))
 }
+
+func TestLinkedListCycleLength(t *testing.T) {
+	node6 := &Node{val: 6}
+	list := Node{
+		val: 1,
+		next: &Node{
+			val: 2,
+			next: &Node{
+				val: 3,
+				next: &Node{
+					val: 4,
+					next: &Node{
+						val:  5,
+						next: node6,
+					},
+				},
+			},
+		},
+	}
+	node6.next = list.next.next
+	assert.Equal(t, 4, LinkedListCycleLength(&list))
+}
