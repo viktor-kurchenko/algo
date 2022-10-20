169ee7bb440e9df152bf616d37ef832bd6ee6edd
ViktorKurchenko <extvikur@pandora.net>
2022-10-20 06:52:56 +0300

    Missing number.

--- 5-cyclic-sort/cyclic_sort.go
+++ 5-cyclic-sort/cyclic_sort.go
@@ -8,3 +8,20 @@ func CyclicSort(a []int) []int {
 	}
 	return a
 }
+
+func MissingNumber(a []int) int {
+	for i := 0; i < len(a); i++ {
+		for a[i] != i {
+			if a[i] == len(a) {
+				break
+			}
+			a[i], a[a[i]] = a[a[i]], a[i]
+		}
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != i {
+			return i
+		}
+	}
+	return -1
+}
--- 5-cyclic-sort/cyclic_sort_test.go
+++ 5-cyclic-sort/cyclic_sort_test.go
@@ -13,3 +13,9 @@ func TestCyclicSort(t *testing.T) {
 
 	assert.Equal(t, []int{1, 2, 3, 4, 5, 6}, CyclicSort([]int{1, 5, 6, 4, 3, 2}))
 }
+
+func TestMissingNumber(t *testing.T) {
+	assert.Equal(t, 2, MissingNumber([]int{4, 0, 3, 1}))
+
+	assert.Equal(t, 7, MissingNumber([]int{8, 3, 5, 2, 4, 6, 0, 1}))
+}
