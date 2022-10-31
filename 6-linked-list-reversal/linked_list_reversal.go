ef73645f6acd322377ac1758f1c9d1b09010e85f
ViktorKurchenko <extvikur@pandora.net>
2022-10-31 07:17:13 +0200

    Reverse SubList.

--- 6-linked-list-reversal/linked_list_reversal.go
+++ 6-linked-list-reversal/linked_list_reversal.go
@@ -15,6 +15,24 @@ func ReverseLinkedList(l *Node) *Node {
 	return node
 }
 
+func ReverseSubList(l *Node, p, q int) *Node {
+	start := l
+	for i := 1; i < p-1 && start.Next != nil; i++ {
+		start = start.Next
+	}
+
+	var prev *Node
+	head := start.Next
+	for i := p; i <= q && head.Next != nil; i++ {
+		next := head.Next
+		head.Next = prev
+		prev, head = head, next
+	}
+
+	start.Next, start.Next.Next = prev, head
+	return l
+}
+
 type Node struct {
 	Val  int
 	Next *Node
--- 6-linked-list-reversal/linked_list_reversal_test.go
+++ 6-linked-list-reversal/linked_list_reversal_test.go
@@ -22,3 +22,40 @@ func TestReverseLinkedList(t *testing.T) {
 		l = l.Next
 	}
 }
+
+func TestReverseSubList(t *testing.T) {
+	actual := ReverseSubList(&Node{
+		Val: 1,
+		Next: &Node{
+			Val: 2,
+			Next: &Node{
+				Val: 3,
+				Next: &Node{
+					Val: 4,
+					Next: &Node{
+						Val: 5,
+					},
+				},
+			},
+		},
+	}, 2, 4)
+	expected := &Node{
+		Val: 1,
+		Next: &Node{
+			Val: 4,
+			Next: &Node{
+				Val: 3,
+				Next: &Node{
+					Val: 2,
+					Next: &Node{
+						Val: 5,
+					},
+				},
+			},
+		},
+	}
+	for i := 0; i < 5; i++ {
+		assert.Equal(t, expected.Val, actual.Val)
+		actual, expected = actual.Next, expected.Next
+	}
+}
